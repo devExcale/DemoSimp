51bab85fad625e08e8b4480dbe6d357579195c7e
Excale <dev_excale@hotmail.com>
2022-05-03 12:02:17 +0200

    Init commit

---
+++ src/main/java/org/experimentalplayers/demosimp/DemoSimpApplication.java
@@ -0,0 +1,13 @@
+package org.experimentalplayers.demosimp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSimpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSimpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/experimentalplayers/demosimp/DemoSimpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.experimentalplayers.demosimp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSimpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
