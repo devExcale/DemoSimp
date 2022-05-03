3117c03dfcff74f18a268d3e3064fd11996d1d5c
Excale <dev_excale@hotmail.com>
2022-05-03 12:59:53 +0200

    Set up bot service

---
+++ src/main/java/org/experimentalplayers/demosimp/DemoSimpBot.java
@@ -0,0 +1,18 @@
+package org.experimentalplayers.demosimp;
+
+import com.pengrad.telegrambot.TelegramBot;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoSimpBot {
+
+	private final TelegramBot bot;
+
+	public DemoSimpBot(@Value("${env.TG_TOKEN}") String token) {
+
+		bot = new TelegramBot(token);
+
+	}
+
+}
